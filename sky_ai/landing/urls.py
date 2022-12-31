444757b9719cbff88052f696e61f639bf85d3da3
huzaifa-salih <hodaeyfha@gmail.com>
2023-01-01 01:10:03 +0300

    templates disgen

--- sky_ai/landing/urls.py
+++ sky_ai/landing/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="index"),
+    path("pricing/", views.pricing, name="pricing"),
+    
 ]
--- sky_ai/landing/views.py
+++ sky_ai/landing/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def home(request):
      return render(request, 'landing/index.html', {})
+
+
+
+def pricing(request):
+     return render(request, 'landing/pricing.html', {})
