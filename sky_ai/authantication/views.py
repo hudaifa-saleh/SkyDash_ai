9ae9cf7d8aa678b1c811b8aa30a412c52d5a8c50
huzaifa-salih <hodaeyfha@gmail.com>
2023-01-01 03:28:35 +0300

    the registerd view

--- sky_ai/authantication/views.py
+++ sky_ai/authantication/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+# from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def login(request):
@@ -6,4 +11,22 @@ def login(request):
 
 
 def register(request):
+    if request.method == "POST":
+        email = request.POST["email"].replace("", "").lower()
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+
+        if not password1 == password2:
+            messages.error(request, "Please enter the correct password")
+            return redirect("register")
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "That email address is already registered")
+            return redirect("register")
+
+        newUser = User.objects.create_user(email=email, username=email, password=password2)
+        newUser.save()
+        auth.login(request, newUser)
+        return redirect("index")
+
     return render(request, "auth/auth-register-basic.html", {})
--- sky_ai/sky_ai/settings.py
+++ sky_ai/sky_ai/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,6 +140,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "info",
+    messages.INFO: "info",
+    messages.SUCCESS: "success",
+    messages.WARNING: "warning",
+    messages.ERROR: "danger",
+}
+
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
